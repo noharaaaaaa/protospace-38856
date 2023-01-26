c412f3d3cdcc6a1583b010ed2dd622d918501eff
noharaaaaaa <97237399+noharaaaaaa@users.noreply.github.com>
2023-01-26 18:22:01 +0900

    users/showのビュー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find(params[:id])
-    @name = user.name
-    @profile = user.profile
-    @occupation = user.occupation
-    @position = user.position
-    @prototypes = Prototype.includes(:user)
+    @user = User.find(params[:id])
+    @prototypes = @user.prototypes
   end
 end
